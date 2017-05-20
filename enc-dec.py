493941df6a4dc54fcd94e3b172c748ad3c0a9f84
bimodh <bimodh@gmail.com>
2017-05-20 15:16:55 -0700

    Update enc-dec.py

--- enc-dec.py
+++ enc-dec.py
@@ -1,4 +1,10 @@
 #Send a list between two systems, by encoding and decoding
+#Add the length of the word as the first and then append the word, concatinate the words to form the string, then send the string
+#Recievers side, read the size of the string, then read the word.
+#Word can contain numbers, special characters etc.
+#So, add the length of the word, add a space, then add the word
+#input = ['aa,.', 'iewrew546p', '6g70']
+#Formated string will be "4 aa,.10 iewrew546p4 6g70"
 
 def encode():
     inpList = ['aa,.', 'iewrew546p', '6g70']#'aa,.', ,
